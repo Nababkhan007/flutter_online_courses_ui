fe7490259b32a20dbe76e056c37cab311fe71b84
Nababkhan007 <markohenry46@gmail.com>
2022-12-22 02:51:44 +0600

    Add light and dark theme static method

--- lib/common/helper/theme_helper.dart
+++ lib/common/helper/theme_helper.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:online_courses_ui/common/helper/color_helper.dart';
 
 class ThemeHelper {
-  static ThemeData get theme => ThemeData(
-        fontFamily: "Poppins",
-        primarySwatch: Colors.blueGrey,
-      ).copyWith(
+  static ThemeData get lightTheme => ThemeData(
+        fontFamily: "Roboto",
+        primaryColor: Colors.deepOrangeAccent,
+        secondaryHeaderColor: Colors.orangeAccent,
+        disabledColor: Colors.white10,
+        backgroundColor: Colors.white54,
+        errorColor: Colors.red,
+        hintColor: Colors.white30,
+        cardColor: Colors.white,
+        textTheme: const TextTheme(
+          headline3: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline5: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 12.0,
+          ),
+        ),
         cardTheme: const CardTheme(
           shadowColor: Colors.transparent,
           shape: RoundedRectangleBorder(
@@ -15,36 +43,67 @@ class ThemeHelper {
             ),
           ),
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            elevation: MaterialStateProperty.all(0.0),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(
-              ColorHelper.catalogueButtonColor,
-            ), // Button color
-            foregroundColor: MaterialStateProperty.all<Color>(
-              Colors.white,
-            ), // Text and icon color
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFFEF7822),
+          secondary: Color(0xFFEF7822),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color(0xFFEF7822),
           ),
         ),
       );
 
-  static void statusAndBottomBarColor({
-    Color statusBarColor = Colors.transparent,
-    Brightness statusBarIconBrightness = Brightness.dark,
-    Color systemNavigationBarColor = Colors.white,
-    Brightness systemNavigationBarIconBrightness = Brightness.dark,
-  }) =>
-      SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-          statusBarColor: statusBarColor,
-          statusBarIconBrightness: statusBarIconBrightness,
-          systemNavigationBarColor: systemNavigationBarColor,
-          systemNavigationBarIconBrightness: systemNavigationBarIconBrightness,
+  static ThemeData get darkTheme => ThemeData(
+        fontFamily: "Roboto",
+        primaryColor: Colors.deepOrangeAccent,
+        secondaryHeaderColor: Colors.orangeAccent,
+        disabledColor: Colors.black12,
+        backgroundColor: Colors.black54,
+        errorColor: Colors.red,
+        hintColor: Colors.black12,
+        cardColor: Colors.black,
+        textTheme: const TextTheme(
+          headline3: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline5: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 12.0,
+          ),
+        ),
+        cardTheme: const CardTheme(
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+        ),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFFFFBD5C),
+          secondary: Color(0xFFFFBD5C),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color(0xFFFFBD5C),
+          ),
         ),
       );
 }
